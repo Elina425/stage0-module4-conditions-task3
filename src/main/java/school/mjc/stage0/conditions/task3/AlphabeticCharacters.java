a1f9dc240082eb0df94e1588c642ae567ce45b3a
Elina <elina.melkonyan.y@tumo.org>
2022-08-03 02:30:09 +0400

    ))

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character=='a'|| character=='i' || character =='e' || character=='o'|| character=='u'){
+            System.out.println("Vowel");
+        }else if((character>='a') && (character<='z')){
+            System.out.println("Consonant");
+        }else{
+            System.out.println("wrong alphabet");
+        }
     }
+
 }
